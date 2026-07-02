15e38f1a5d725ed38d6f755bb56745331d673aaa
FloatibleCrib86 <140321362+FloatibleCrib86@users.noreply.github.com>
2026-07-02 11:47:56 +1000

    Update service-worker.js

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,13 +1,16 @@
-const CACHE_NAME = "solo-system-public-mobile-v9";
+const CACHE_NAME = "solo-system-public-mobile-v11";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", event => {
+    self.skipWaiting();
     event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+        self.clients.claim().then(() =>
+            caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+        )
     );
 });
 
